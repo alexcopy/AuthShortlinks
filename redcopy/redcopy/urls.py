297f8f29a4e7b214d1386e0a141344f78b667fe6
Alex Redko <alexredko@redcopy.local>
2021-08-10 15:10:00 +0100

    fix urls issues

--- redcopy/redcopy/urls.py
+++ redcopy/redcopy/urls.py
@@ -5,10 +5,10 @@ from shorturlsapp import views
 urlpatterns = [
     path('accounts/', include('accounts.urls')),
     # path('accounts/login/', templates.LoginView.as_view(template_name='myapp/login.html')),
+    path('short/', include('shorturlsapp.urls')),
     path('/', include('shorturlsapp.urls')),
     path('', include('shorturlsapp.urls')),
     path('admin/', admin.site.urls),
-    path('short', include('shorturlsapp.urls')),
-    re_path(r'(?P<rnd_string>\w{5})$', views.shorturl ),
+    path('<str:rnd_string>/', views.shorturl),
 
 ]
--- redcopy/shorturlsapp/urls.py
+++ redcopy/shorturlsapp/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('redirect/', views.short_url, name='redirect'),
-    re_path(r'redirect/(?P<rnd_string>\w+)/$', views.short_url, name='redirect'),
+    re_path(r'redirect/(?P<rnd_string>\w{5,5})/$', views.short_url, name='redirect'),
     path('delete/<url_id>/', views.delete_shorturl, name='delete_post'),
 ]
