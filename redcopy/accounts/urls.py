4094823d1dfd087c97693dcde6335dcb70c9f08f
Alex Redko <alexredko@redcopy.local>
2021-08-14 16:17:39 +0100

    fix logout url

--- redcopy/accounts/urls.py
+++ redcopy/accounts/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from .views import login_view, register_view, logout_view
 
 urlpatterns = [
-    path('login/', login_view),
+    path('login/', login_view, name="login"),
     path('login', login_view),
-    path('register/', register_view),
+    path('register/', register_view, name="register"),
     path('register', register_view),
     path('logout/', logout_view, name='logout'),
 
