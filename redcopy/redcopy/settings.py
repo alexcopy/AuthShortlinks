7d6f79818cc088a2e6a6d3823aa802cb65f1e8f1
Alex Redko <a.redko@nr-online.com>
2022-11-29 16:50:39 +0000

    hardocode static

--- redcopy/redcopy/settings.py
+++ redcopy/redcopy/settings.py
@@ -115,10 +115,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
-STATICFILES_DIRS = (
-    os.path.join(SITE_ROOT, 'static/'),
-)
+SITE_ROOT = "/home/redcopy/py_hill/redcopy/static/"
+
 STATIC_URL = '/static/'
 
 # Default primary key field type
